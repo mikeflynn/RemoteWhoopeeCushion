a58b778449b8bfd92845a683dedca10c3a2401fd
Mike Flynn <mfflynn@gmail.com>
2014-12-20 01:01:11 -0800

    Initial addition of the new activity and first version of the layout.

---
+++ mobile/src/main/java/io/github/mikeflynn/remotewhoopeecushion/RecordingsActivity.java
@@ -0,0 +1,16 @@
+package io.github.mikeflynn.remotewhoopeecushion;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class RecordingsActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recordings);
+    }
+}
